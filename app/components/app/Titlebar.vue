cd0ceac43f31e4465c47a234f793492082ce2f38
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-06-26 19:31:50 +0800

    feat(components): add titlebar

---
+++ app/components/app/Titlebar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import { name, version } from '~~/package.json';
+</script>
+
+<template>
+  <div
+    class="flex h-8.25 items-center gap-2 border-b border-muted bg-muted ps-20 select-none"
+    data-tauri-drag-region
+  >
+    <p class="text-xs">
+      {{ capitalize(name) }}
+    </p>
+
+    <UBadge
+      :label="version"
+      size="xs"
+      variant="subtle"
+    />
+  </div>
+</template>
