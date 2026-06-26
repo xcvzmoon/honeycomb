402a651a8075b737a165f607d22337d74753395b
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-06-26 19:32:53 +0800

    feat(layouts): add authenticated

---
+++ app/layouts/authenticated.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col bg-default">
+    <AppTitlebar />
+
+    <div class="flex h-[calc(100svh-2.0625rem)]">
+      <AppSidebar />
+
+      <div class="flex-1">
+        <AppHeader />
+
+        <AppScrollArea class="h-[calc(100svh-6.125rem)]">
+          <NuxtPage />
+        </AppScrollArea>
+      </div>
+    </div>
+  </div>
+</template>
