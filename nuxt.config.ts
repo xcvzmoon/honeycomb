c218c9add2d48861c49a46b54d68e673d4f28a5d
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-06-26 19:27:06 +0800

    chore: comply to tauri's nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,21 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
   compatibilityDate: '2026-06-26',
+  ssr: false,
   devtools: {
     enabled: true,
   },
+  devServer: {
+    host: '0',
+    port: 3000,
+  },
   vite: {
+    clearScreen: false,
     plugins: [tailwindcss()],
+    envPrefix: ['VITE_', 'TAURI_'],
+    server: {
+      strictPort: true,
+    },
     optimizeDeps: {
       include: ['vue', 'vue-router', '@vue/devtools-core', '@vue/devtools-kit'],
     },
